2f1c20c16c3032c305cb48bdead3ed2965e4ef5a
DevLuizP <contato.luizpcardoso@gmail.com>
2022-11-09 14:44:25 -0300

    arquivo: cadastraClientes-controller.js
    corrigida a ausência do fechamento de dois colchetes
    variaveis corrigidas: nome e email
    linhas: 8 e 9

--- projeto_inicial/admin/controller/cadastraClientes-controller.js
+++ projeto_inicial/admin/controller/cadastraClientes-controller.js
@@ -5,8 +5,8 @@ const formulario = document.querySelector('[data-form]')
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
-    const nome = event.target.querySelector('[data-nome').value
-    const email = event.target.querySelector('[data-email').value
+    const nome = event.target.querySelector('[data-nome]').value
+    const email = event.target.querySelector('[data-email]').value
 
     clienteService.criaCliente(nome, email)
     .then(() => {
